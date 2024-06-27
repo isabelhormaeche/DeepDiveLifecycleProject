52c0b1bb1a4e72f7fbf6b40c43758d18fc7c3e72
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-06-27 19:41:47 +0200

    Updated workflow.js and discussion.js files

--- src/components/discussion.js
+++ src/components/discussion.js
@@ -1,9 +1,36 @@
 import React, { Component} from "react";
 
 export default class Discussion extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            pageTitle: "Discussion",
+            currentTime: String(new Date())
+        };
+    }
+
+    componentDidMount() {
+        this.liveTime = setInterval(() => { 
+            console.log("New chat message");
+            this.setState({currentTime: String(new Date()) });
+        }, 1000);
+    }
+
+
+    componentWillUnmount() {
+        clearInterval(this.liveTime);
+        console.log("componentWillUnmount");
+      }
+      
     render() {
+        const { pageTitle, currentTime} = this.state;
         return (
-            <div>Discussion...</div>
+            <div>
+                <h1>{pageTitle}</h1>
+                <div>{currentTime}</div>
+
+            </div>
         );
     }
 }
